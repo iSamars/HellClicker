ad4e3ff6e1eb3547f793ad17df5070983664478d
Mr SaMaRs <Mr SaMaRs>
2017-07-21 14:05:29 +0700

    xx xxx

---
+++ Assets/Scripts/MusicOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+public class MusicOnClick : MonoBehaviour {
+	public void MusicOnClics()
+	{
+		GetComponent<AudioSource>().Play();
+	}
+}
